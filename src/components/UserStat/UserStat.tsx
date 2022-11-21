4c2ab15aa9e6159b207b01fd25266d75d6745abf
ilonaK <ikosko@alfabank.ru>
2022-11-21 23:31:33 +0300

    refactor: refactor UserStat component

--- src/components/UserStat/UserStat.tsx
+++ src/components/UserStat/UserStat.tsx
@@ -6,10 +6,23 @@ import styles from './UserStat.module.scss';
 
 export type UserStatProps = Pick<LocalGithubUser, 'repos' | 'followers' | 'following'>;
 
-export const UserStat = ({ repos, followers, following }: UserStatProps) => (
-  <div className={styles.userStat}>
-    <UserStatItem content={repos} label='Repos' />
-    <UserStatItem content={followers} label='Followers' />
-    <UserStatItem content={following} label='Following' />
-  </div>
-);
+type Items<T> = {
+  content: T;
+  label: string;
+};
+
+export const UserStat = ({ repos, followers, following }: UserStatProps) => {
+  const items: Items<typeof repos | typeof followers | typeof following>[] = [
+    { content: repos, label: 'Repos' },
+    { content: followers, label: 'Followers' },
+    { content: following, label: 'Following' },
+  ];
+
+  return (
+    <div className={styles.userStat}>
+      {items.map((item) => (
+        <UserStatItem {...item} />
+      ))}
+    </div>
+  );
+};
