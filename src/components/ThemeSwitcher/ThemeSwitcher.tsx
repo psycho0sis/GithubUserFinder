1f9671d423516a254a021f6be2a692f572360a26
ilonaK <ikosko@alfabank.ru>
2022-11-22 00:07:07 +0300

    feat: add theme to localStorage

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+/* eslint-disable no-console */
+import { useLayoutEffect, useState } from 'react';
 
 import { ReactComponent as MoonIcon } from 'assets/icon-moon.svg';
 import { ReactComponent as SunIcon } from 'assets/icon-sun.svg';
@@ -6,12 +7,18 @@ import { ReactComponent as SunIcon } from 'assets/icon-sun.svg';
 import styles from './ThemeSwitcher.module.scss';
 
 export const ThemeSwitcher = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    const theme = window.localStorage.getItem('theme');
+    const initialValue = theme === 'light' ? false : true;
+    return initialValue;
+  });
+
   const themeText = isDark ? 'Light' : 'Dark';
   const ThemeIcon = isDark ? MoonIcon : SunIcon;
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
+    window.localStorage.setItem('theme', isDark ? 'dark' : 'light');
   }, [isDark]);
 
   return (
